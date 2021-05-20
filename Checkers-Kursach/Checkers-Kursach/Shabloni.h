47d99ced5d3da51f42fc2f024dce90f4190a54cd
Yarik <yaroslav.matsapura@gmail.com>
2021-05-21 00:30:02 +0300

    Almost ready 2 person version but with many errors

--- Checkers-Kursach/Checkers-Kursach/Shabloni.h
+++ Checkers-Kursach/Checkers-Kursach/Shabloni.h
@@ -1,3 +1,9 @@
 #pragma once
 #include <array>
+#include <vector>
+#include <memory>
+
+class Move;
 typedef std::array<int, 2> coordinates;
+typedef std::vector<std::shared_ptr<Movement>> all_moves_vector;
+typedef std::shared_ptr<Movement> pointer_to_move;
--- Checkers-Kursach/Checkers-Kursach/Source.cpp
+++ Checkers-Kursach/Checkers-Kursach/Source.cpp
@@ -1,5 +1,63 @@
 #include <iostream>
+#include "Board_Checkers.h"
+#include "Cell.h"
+#include "Movement.h"
+#include "Player.h"
+#include "Shabloni.h"
+#include <vector>
+#include <iostream>
+bool isPlayer1 = true;
+
+bool game_finish(const Board_Checkers board_checkers) {
+	int whiteQuant = 0;
+	int blackQuant = 0;
+	for (int pos = 0; pos < Board_Checkers::size*Board_Checkers::size; pos++)
+	{
+		Cell* current = board_checkers.getColor(pos);
+		if (current == nullptr) {
+			if (!current->make_move_vector(board_checkers).empty()) {
+				if (current->isWhite) {
+					whiteQuant++;
+				}
+				else {
+					blackQuant++;
+				}
+			}
+		}
+
+	}
+
+
+	if (whiteQuant + blackQuant == 0) {
+		std::cout << "Tie" << std::endl;
+	}
+	else if (whiteQuant == 0) {
+		std::cout << "Black won" << std::endl;
+	}
+	else if (blackQuant == 0) {
+		std::cout << "White won" << std::endl;
+	}
+	else {
+		return false;
+	}
+	return true;
+
+}
 
 int main() {
+	Board_Checkers board_checkers;
+	Player* player1;
+	Player* player2;
+	while (!game_finish(board_checkers)) {
+		if (isPlayer1) {
+			player1->form_move(board_checkers);
+		}
+		else {
+			player2->form_move(board_checkers);
+		}
+
+		isPlayer1 = !isPlayer1;
+	}
+
 
 }
